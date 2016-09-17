bffad32a144f23867fb78a2c24b45a0a1c1018cd
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-09-17 03:09:52 +0200

    Adding base featurecomputer

--- src/main/java/marytts/features/FeatureComputer.java
+++ src/main/java/marytts/features/FeatureComputer.java
@@ -1,6 +1,6 @@
 package marytts.features;
 
-
+import marytts.data.SupportedSequenceType;
 import marytts.data.Utterance;
 import marytts.data.item.Item;
 import java.util.Hashtable;
@@ -12,11 +12,20 @@ import java.util.Hashtable;
  */
 public class FeatureComputer
 {
-    private Hashtable<String, String[]> m_features;
+    protected Hashtable<String, String[]> m_features;
 
-    public FeatureComputer()
+    protected ContextProcessorFactory m_context_factory;
+    protected LevelProcessorFactory m_level_factory;
+    protected FeatureProcessorFactory m_feature_factory;
+
+    public FeatureComputer(LevelProcessorFactory level_factory,
+                           ContextProcessorFactory context_factory,
+                           FeatureProcessorFactory feature_factory)
     {
         m_features = new Hashtable<String, String[]>();
+        m_context_factory = context_factory;
+        m_feature_factory = feature_factory;
+        m_level_factory = level_factory;
     }
 
     public void addFeature(String name, String level, String context, String feature)
@@ -25,11 +34,25 @@ public class FeatureComputer
     }
 
     public Feature compute(Utterance utt, Item item, String level, String context, String feature)
+        throws Exception // FIXME: be more specific
     {
-        return null;
+        LevelProcessor level_processor = m_level_factory.createLevelProcessor(level);
+        Item[] level_items = level_processor.generate(utt, item);
+        if (level_items.length == 0)
+            return null;
+
+        ContextProcessor context_processor = m_context_factory.createContextProcessor(context);
+        Item[] context_items = context_processor.generate(utt, level_items[0]);
+
+        if (context_items.length == 0)
+            return null;
+
+        FeatureProcessor feature_processor = m_feature_factory.createFeatureProcessor(feature);
+        return feature_processor.generate(utt, context_items[0]);
     }
 
     public FeatureMap process(Utterance utt, Item item)
+        throws Exception // FIXME: be more specific
     {
         FeatureMap feature_map = new FeatureMap();
         for (String feature_name: m_features.keySet())
