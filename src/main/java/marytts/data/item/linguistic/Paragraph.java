5f69540195a6a3b56bf2d327e737371a8169f755
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2016-09-23 05:09:54 +0200

    Adding some comments

--- src/main/java/marytts/data/item/linguistic/Paragraph.java
+++ src/main/java/marytts/data/item/linguistic/Paragraph.java
@@ -5,25 +5,40 @@ import java.util.ArrayList;
 import marytts.data.item.Item;
 
 /**
- *
+ * Class which represents a paragraph
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
  */
 public class Paragraph extends Item
 {
-	private String m_text;
+	private String m_text; /*< For now the paragraph is just containing a text */
 
+    /**
+     *  Constructor of the paragrapj
+     *
+     *  @param text : the text of the paragraph
+     */
 	public Paragraph(String text)
 	{
         super();
         setText(text);
     }
 
+    /**
+     *  Getter of the text of the Paragraph
+     *
+     *  @return the text of the paragraph
+     */
     public String getText()
 	{
 		return m_text;
 	}
 
+    /**
+     *  Setter of the text of the Paragraph
+     *
+     *  @param the new text of the Paragraph
+     */
 	protected void setText(String text)
 	{
 		m_text = text;
--- src/main/java/marytts/data/item/linguistic/Sentence.java
+++ src/main/java/marytts/data/item/linguistic/Sentence.java
@@ -6,25 +6,40 @@ import marytts.data.item.Item;
 import marytts.data.item.prosody.Phrase;
 
 /**
- *
+ * Class which represents a sentence
  *
  * @author <a href="mailto:slemaguer@coli.uni-saarland.de">Sébastien Le Maguer</a>
  */
 public class Sentence extends Item
 {
-	private String m_text;
+	private String m_text; /*< For now the sentence is just containing a text */
 
+    /**
+     *  Constructor of the paragrapj
+     *
+     *  @param text : the text of the sentence
+     */
 	public Sentence(String text)
 	{
         super();
 		setText(text);
 	}
 
+    /**
+     *  Getter of the text of the Sentence
+     *
+     *  @return the text of the sentence
+     */
 	public String getText()
 	{
 		return m_text;
 	}
 
+    /**
+     *  Setter of the text of the Sentence
+     *
+     *  @param the new text of the Sentence
+     */
 	protected void setText(String text)
 	{
 		m_text = text;
